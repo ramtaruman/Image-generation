1f762255cff26b87b8886d0f7ed1d3a873eb9921
ramtaruman <sengupta.prakhar@gmail.com>
2023-04-20 22:26:41 +0530

    Finalized changes

--- public/js/main.js
+++ public/js/main.js
@@ -4,27 +4,26 @@ function onSubmit(e) {
     document.querySelector('.msg').textContent = '';
     document.querySelector('#image').src = '';
 
-    const init_prompt = document.getElementById('prompt').value;
-    console.log(init_prompt);
-    const style_prompt = document.getElementById('style-prompt').value;
+    const init_prompt = document.querySelector('#prompt').value;
+    const type_prompt = document.getElementById('imgtyp').value;
+    console.log(type_prompt);
+    const style_prompt = document.getElementById('imgstyl').value;
     console.log(style_prompt);
-    const color_prompt = document.getElementById('color-prompt').value;
+    const color_prompt = document.getElementById('imgclr').value;
     console.log(color_prompt);
+    const content_prompt = document.getElementById('imgcnt').value;
+    console.log(content_prompt);
+    const mood_prompt = document.getElementById('imgmod').value;
+    console.log(mood_prompt);
+    const bg_prompt = document.getElementById('imgbg').value;
+    console.log(bg_prompt);
     const size = document.querySelector('#size').value;
 
     if (init_prompt === '') {
       alert('Please add some text');
       return;
     }
-    if (style_prompt === '') {
-      alert('Please add a style');
-      return;
-    }
-    if (color_prompt === '') {
-      alert('Please add a color palette');
-      return;
-    }
-    const prompt = init_prompt + ' in the style of ' + style_prompt + ' with a ' + color_prompt +' tone '
+    const prompt = init_prompt + ' in a setting of ' + type_prompt + ', in a ' + style_prompt +' style, with a color scheme of  ' + color_prompt + ' having content of ' + content_prompt + ' with ' + mood_prompt + ' mood and with background ' +bg_prompt
     generateImageRequest(prompt, size);
   }
 
